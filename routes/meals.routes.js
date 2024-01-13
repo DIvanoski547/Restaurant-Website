8c355fb7c931cac77181ce6737eb7d9215dfa311
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-13 11:08:01 +0100

    add { userInSession: req.session.currentUser } to /meal route

--- routes/meals.routes.js
+++ routes/meals.routes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 const Meal = require("../models/Meal.model");
 
-/* -------- MENU PAGE --------*/
+/*-----GET MENU PAGE-----*/
 router.get("/menu", (req, res, next) => {
     res.render("meals/menu", { userInSession: req.session.currentUser } );
   });
 
+/*-----SINGLE MEAL-----*/
 router.get("/meal", (req, res, next) => {
-    res.render("meals/meal")
+    res.render("meals/meal", { userInSession: req.session.currentUser })
 })
 
   module.exports = router;
