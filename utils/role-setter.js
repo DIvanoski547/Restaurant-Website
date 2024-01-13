bd62e3b471c3e5d6f495d0059a87dc672dfc1d8d
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-13 12:09:52 +0100

    remove role-setter.js, no longer needed, logic implemented directly into route-guard.js

--- utils/role-setter.js
+++
@@ -1,9 +0,0 @@
-function roleSetter(req, res, next){
-    // Set the role to 'admin', 'moderator', or 'customer'
-    // by getting the `role` attribute from the request headers
-    req.user =  { role: req.get('role') };
-    next();
-}
-
-
-module.exports = roleSetter;
