8c213387ec0a52f3bdbecabaf3be12bf929136f9
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-13 11:13:13 +0100

    remove require isLoggedIn and isLoggedOut middlewares

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Require necessary middleware to control access to specific routes
-const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
-
 /*-----GET HOME PAGE-----*/
 router.get("/", (req, res, next) => {
   res.render("index", { userInSession: req.session.currentUser });
