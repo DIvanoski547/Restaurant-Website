441165f04a0499ce49cf0c650266199dfc06d2db
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-16 20:49:30 +0100

    add route to create new meal

--- routes/meals.routes.js
+++ routes/meals.routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Require necessary middleware to control access to specific routes
+const { isLoggedIn, isLoggedOut, isAdmin, isAdminOrModerator } = require('../middleware/route-guard.js');
+
+// Require the Meal model in order to interact with the database
 const Meal = require("../models/Meal.model");
 
 /*-----GET MENU PAGE-----*/
@@ -8,9 +12,15 @@ router.get("/menu", (req, res, next) => {
     res.render("meals/menu", { userInSession: req.session.currentUser } );
   });
 
-/*-----SINGLE MEAL-----*/
+/*-----GET SINGLE MEAL-----*/
 router.get("/meal", (req, res, next) => {
     res.render("meals/meal", { userInSession: req.session.currentUser })
-})
+});
+
+/*-----GET CREATE MEAL-----*/
+// display the form which allows new meals to be created
+router.get('/meals/create', isAdmin, (req, res) => {
+  res.render('meals/new-meal', { userInSession: req.session.currentUser })
+});
 
   module.exports = router;
