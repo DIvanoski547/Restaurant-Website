5e08917df06511460d53d1dc8e19f73183e2fa43
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-16 21:32:36 +0100

    add create, show all meals, show one meal, edit/update, delete routes

--- routes/meals.routes.js
+++ routes/meals.routes.js
@@ -23,4 +23,113 @@ router.get('/meals/create', isAdmin, (req, res) => {
   res.render('meals/new-meal', { userInSession: req.session.currentUser })
 });
 
+/*-----POST CREATE MEAL-----*/
+// route to create a new meal using data submitted via form
+router.post('/meals/create', isAdmin, (req, res, next) => {
+  console.log('New meal added via online form:', req.body);
+  const { name, ingredients, allergens, spiceLevel, mealImage, category, quisine, dishType } = req.body;
+
+  // Check that a name, ingredients and allergens have been provided
+  if (name === "" || ingredients === "" || allergens === "") {
+    console.log("Either a name, ingredients or allergens, or all three, have not been provided.")
+    res.render(
+      "meals/new-meal",
+      { errorMessage: "All fields are mandatory. Please provide a name, ingredients and allergens." }
+      );
+    return;
+  };
+
+  Meal.findOne({ name })
+      .then((foundMeal) => {
+        if (!foundMeal) {
+          Meal.create({ name, ingredients, allergens, spiceLevel, mealImage, category, quisine, dishType })
+              .then(() => res.redirect("/meals"));
+              console.log('New meal successfully added to database.')
+      } 
+          else {
+              res.render("meals/new-meal", {
+              errorMessage: "This meal already exists in the database."
+              });
+              console.log('The meal has not been added to the database. Meal already exists.');
+              return;
+          }
+      })
+      .catch(error => {
+          console.log('Error creating new meal: ', error);
+          next(error);
+      });
+});
+
+/*-----GET ALL MEALS-----*/
+// route to display a list of all meals found in the database
+router.get('/meals', isAdmin, (req, res, next) => {
+  Meal.find()
+      .then((allMeals) => {
+          res.render('meals/all-meals', { allMeals, userInSession: req.session.currentUser })
+          console.log(`There are currently ${allMeals.length} users in the database.`);
+      })
+      .catch(error => {
+          console.log('Error while displaying list of all users: ', error);
+          next(error);
+      });
+});
+
+/*-----GET SINGLE MEAL-----*/
+// route to display a specific meal on the meal-details page
+router.get('/meals/:mealId', isAdmin, (req, res, next) => {
+  const { mealId } = req.params;
+
+  Meal.findById(mealId)
+      .then(foundMeal => res.render('meals/meal-details', { foundMeal, userInSession: req.session.currentUser }))
+      .catch(error => {
+          console.log('Error while retrieving meal details: ', error);
+          next(error);
+      });
+});
+
+/*-----POST DELETE MEAL-----*/
+// route to delete a specific meal from the database
+router.post('/meals/:mealId/delete', isAdmin, (req, res, next) => {
+  const { mealId } = req.params;
+
+  Meal.findByIdAndDelete(mealId)
+      .then(() => res.redirect('/meals'))
+      .catch(error => {
+          console.log('Error while deleting meal: ', error);
+          next(error);
+      });
+});
+
+/*-----GET EDIT ANY MEAL-----*/
+// route to find the meal we would like to edit in the database
+// show a pre-filled form to update a meal's info
+router.get('/meals/:mealId/edit', isAdmin, (req, res, next) => {
+  const { mealId } = req.params;
+
+  Meal.findById(mealId)
+      .then((foundMeal) => res.render('meals/edit-meal', { foundMeal }))
+      .catch(error => {
+          console.log('Error while updating meal: ', error);
+          next(error);
+      });
+});
+
+/*-----POST UPDATE ANY MEAL-----*/
+// route to submit the form to update the meal in the database
+// save the updated meal to the database
+router.post('/meals/:mealId/edit', isAdmin, (req, res, next) => {
+  const { mealId } = req.params;
+  const { name, ingredients, allergens, spiceLevel, mealImage, category, quisine, dishType } = req.body;
+
+  Meal.findByIdAndUpdate(mealId, { name, ingredients, allergens, spiceLevel, mealImage, category, quisine, dishType })
+      .then((foundMeal) => {
+          console.log(foundMeal);
+          res.redirect(`/users/${foundMeal._id}`)
+      })
+      .catch(error => {
+          console.log('Error while updating meal: ', error);
+          next(error);
+      });
+});
+
   module.exports = router;
