7c3849e5406f8f17f69834b0beba9c8bb7e23ccd
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-18 18:03:53 +0100

    update meal.seed with 1 mean

--- seeds/meal.seed.js
+++ seeds/meal.seed.js
@@ -3,7 +3,7 @@ const Meal = require("../models/Meal.model");
 
 const meals = [
   { 
-    name: "Spaghetti with tomato sauce",
+  name: "Spaghetti with tomato sauce",
   ingredients: "tomato sauce" ,
   allergens:"gluten",
   spiceLevel: 2,
@@ -11,24 +11,26 @@ const meals = [
   category: "main dish",
   cuisine: "Italian",
   dishType: "lunch",
-
 },
+{ 
+  name: "Tacos",
+  ingredients: "Corn tortillas, fried fish pieces, lettuce, tomatoes, shredded cheddar cheese, sour cream" ,
+  allergens:"lactose",
+  spiceLevel: 0,
+  mealImage: "../public/images/tacos.jpg",
+  category: "main dish",
+  cuisine: "Mexican",
+  dishType: "lunch",
+}
   
 ];
 
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Restaurant-website";
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-    return Meal.create(meals);
-  })
-  .then((allMeals) =>
-    console.log(`${allMeals.length} meals were added`)
-  )
+mongoose.connect(MONGO_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then(() => Meal.create(meals))
+  .then((allMeals) => console.log(`${allMeals.length} meals were added`))
   .then(() => mongoose.connection.close())
   .catch((err) => console.log(err));
