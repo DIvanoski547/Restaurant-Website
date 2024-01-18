4ab41cdd02e517739016fdbe42cddd06587245c9
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-18 21:11:44 +0100

    add title to Comments model

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -2,10 +2,15 @@ const { Schema, model } = require("mongoose");
 
 const commentSchema = new Schema(
   {
+    title: {
+      type: String,
+      required: true
+    },
     dish: [{ type: Schema.Types.ObjectId, ref: "Meal" }],
     author: [{ type: Schema.Types.ObjectId, ref: "User" }],
     content: {
-        type: String
+        type: String,
+        required: true
     }
   },
   {
