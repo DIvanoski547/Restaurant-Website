e11e9548e84e5e0e5cd57ad730807986f916b5b1
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-25 11:57:06 +0100

    change correct path back to original value

--- routes/favorites.routes.js
+++ routes/favorites.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Favorites = require("../models/Favorites.model");
+const Favorites = require("../models/favorites.model");
 const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
 
 app.post('/add-to-favorites', async (req, res) => {
@@ -13,8 +13,6 @@ app.post('/add-to-favorites', async (req, res) => {
     // Redirect back to the main page
     res.redirect('/');
 });
-
-
 
 
 
