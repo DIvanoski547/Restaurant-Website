373dc80272d44724e4fcfcf35d4bc9546e93a9b4
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-25 11:37:11 +0100

    change const favorites to const Favorites and correct path

--- routes/favorites.routes.js
+++ routes/favorites.routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const favorites = require("../models/favorites.model");
+const Favorites = require("../models/Favorites.model");
 const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
 
 app.post('/add-to-favorites', async (req, res) => {
     const { name } = req.body;
 
     // Create a new favorite and save it to the database
-    await Favorite.create({ name });
+    await Favorites.create({ name });
 
     // Redirect back to the main page
     res.redirect('/');
