8012fc8516b9b0fe88f51505371c8684432bf8e5
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-26 17:42:43 +0100

    add additional comments to routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 /*-----GET HOME PAGE-----*/
+// route to display home page and check whether a user is currently logged in
 router.get("/", (req, res, next) => {
   res.render("index", { userInSession: req.session.currentUser });
 });
 
 /*-----GET ABOUT PAGE-----*/
+// route to display about page and check whether a user is currently logged in
 router.get("/about", (req, res, next) => {
   res.render("about", { userInSession: req.session.currentUser });
 });
