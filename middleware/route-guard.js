f619835d5db250ad3e8d6f197ae5a274fcb77196
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-01-26 17:29:11 +0100

    remove isAdminOrModerator middleware, not needed

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -25,20 +25,10 @@ const isAdmin = (req, res, next) => {
   }
   next();
 };
-
-// checks if the user has admin or moderator rights
-const isAdminOrModerator = (req, res, next) => {
-  if (req.session.currentUser.role != "admin" && req.session.currentUser.role != "moderator") {
-    console.log("User needs admin or moderator rights to access this route.");
-    return res.redirect('/login');
-  }
-  next();
-};
   
 module.exports = {
     isLoggedIn,
     isLoggedOut,
-    isAdmin,
-    isAdminOrModerator
+    isAdmin
 };
   
